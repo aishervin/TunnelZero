9ef41655dd4bca4ff42ec3084e607c0a60c39b69
SHΞЯVIN™ <cyphergeminis@gmail.com>
2026-09-24 00:00:46 +0330

    ®️SHΞN™ᴢᴇʀᴏ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
+  splits {
+    abi {
+      isEnable = true
+      reset()
+      include("arm64-v8a", "armeabi-v7a", "x86_64")
+      isUniversalApk = true
+    }
+  }
+
   signingConfigs {
     create("release") {
       val customPath = System.getenv("KEYSTORE_PATH")
